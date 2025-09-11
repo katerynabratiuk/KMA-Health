b1bdd58960a5a6b043196557239bb27417e9e79c
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-09-11 13:00:38 +0300

    Initial commit

---
+++ src/main/java/kma/health/app/kma_health/KmaHealthApplication.java
@@ -0,0 +1,13 @@
+package kma.health.app.kma_health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmaHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmaHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kma/health/app/kma_health/KmaHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package kma.health.app.kma_health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmaHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
