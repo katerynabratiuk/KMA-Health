c17dbe2a2a65291ecc05721af0d0c2327b04e047
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-10-09 20:58:14 +0300

    Implement simple hospital controller, service and repository

--- src/main/java/kma/health/app/kma_health/controller/HospitalController.java
+++ src/main/java/kma/health/app/kma_health/controller/HospitalController.java
@@ -1,11 +1,31 @@
 package kma.health.app.kma_health.controller;
 
+import kma.health.app.kma_health.dto.HospitalDto;
+import kma.health.app.kma_health.service.HospitalService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/hospital")
 public class HospitalController {
+
+    HospitalService hospitalService;
+
+    @GetMapping()
+    public List<HospitalDto> searchHospitals(
+            @RequestParam(required = false) String name,
+//            @RequestParam(required = false) Double lat,
+//            @RequestParam(required = false) Double lon, // TODO
+            @RequestParam() Integer pageSize,
+            @RequestParam() Integer pageNum
+    ){
+        return hospitalService.searchHospitals(name, pageNum, pageSize);
+    }
+
 }
--- src/main/java/kma/health/app/kma_health/dto/HospitalDto.java
+++ src/main/java/kma/health/app/kma_health/dto/HospitalDto.java
@@ -1,5 +1,6 @@
 package kma.health.app.kma_health.dto;
 
+import kma.health.app.kma_health.entity.Hospital;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,13 @@ public class HospitalDto
     private String address;
     private String phoneNumber;
     private String city;
+
+    public static HospitalDto fromEntity(Hospital h) {
+        HospitalDto d = new HospitalDto();
+        d.setName(h.getName());
+        d.setAddress(h.getAddress());
+        d.setCity(h.getCity());
+        return d;
+    }
 }
 
