8f9fbc64a3cf8021fc42b273b3b98408adc4ea74
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-11-26 10:23:09 +0200

    Reconfigure HospitalControllerTest to use @WebMvcTest

--- src/test/java/kma/health/app/kma_health/controllers/HospitalControllerTest.java
+++ src/test/java/kma/health/app/kma_health/controllers/HospitalControllerTest.java
@@ -1,17 +1,17 @@
 package kma.health.app.kma_health.controllers;
 
+import kma.health.app.kma_health.controller.api.HospitalController;
 import kma.health.app.kma_health.dto.HospitalDto;
 import kma.health.app.kma_health.security.JwtUtils;
 import kma.health.app.kma_health.service.HospitalService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -22,8 +22,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(HospitalController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class HospitalControllerTest {
 
     @Autowired
@@ -37,21 +37,18 @@ public class HospitalControllerTest {
 
     @Test
     @WithAnonymousUser
-    public void testGetAllHospitals() throws Exception {
+    void testGetAllHospitals() throws Exception {
         HospitalDto mockHospital = new HospitalDto();
         mockHospital.setId(1L);
         mockHospital.setName("Test Hospital");
 
-        List<HospitalDto> mockHospitals = new ArrayList<>();
-        mockHospitals.add(mockHospital);
-
         when(hospitalService.searchHospitals(anyString(), anyInt(), anyInt()))
-                .thenReturn(mockHospitals);
+                .thenReturn(List.of(mockHospital));
 
         mockMvc.perform(get("/api/hospital")
-                .param("name", "Test")
-                .param("pageNum", "0")
-                .param("pageSize", "20"))
+                        .param("name", "Test")
+                        .param("pageNum", "0")
+                        .param("pageSize", "20"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id").value(1))
